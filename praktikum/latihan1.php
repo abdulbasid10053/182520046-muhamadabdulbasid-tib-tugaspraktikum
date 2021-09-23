3fa432913e0b2597eeddd23c59dd916cf490799b
basiit_abdul <muhamadabdulbasid182520046@gmail.com>
2021-09-23 20:00:57 +0700

    push tugas
    tugas framework beaginier

---
+++ praktikum/latihan1.php
@@ -0,0 +1,22 @@
+<?php
+echo "Menggunakan perintah echo";
+echo "Selamat Datang di Matakuliah Praktikum Pemograman Framework<br>";
+echo "-------------------------------------<br>";
+print("menggunakan perintah print");
+print("Selamat Datang di Matakuliah Praktikum Pemograman Framework<br><br>");
+?>
+<?php
+	$nama = "The Prediksi";
+	$no = 4;
+	$desimal = 0.5;
+
+	echo "<b>Penggunaan echo </b><br>";
+	echo "Nama Kelompok :".$nama."<br>";
+	echo "No : ".$no."<br>";
+	echo "desimal : ".$desimal."<br>";
+
+	print "<b>Penggunaan print<b><br>";
+	print "Nama kelompok : ".$nama."<br>";
+	print " No : ".$no."<br>";
+	print "desimal : ".$desimal."<br>";
+?>
---
+++ praktikum/profile/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+        <link href="./css/main.css" rel="stylesheet" />
+        <title>PROFILE</title>
+    </head>
+    <body>
+    	  <?php 
+  $nim = 182520046;
+  $nama = "Muhamad Abdul Basid";
+  $alamat = "Bandongan, Sendangsari, Bener, Purworejo";
+  $email = "muhamadabdulbasid182520046@gmail.com";
+  ?>
+        <div class="v1_3">
+            <div class="v2_3">
+                <div class="image"></div>
+                <div class="v2_2">
+                    <span class="v1_8">Muhamad Abdul Basid</span><span class="v1_9">Nim : <?php echo $nim;?></span><span class="v1_10">Alamat : <?php echo $alamat;?></span>
+                    <span class="v1_11">Email : <?php echo $email;?></span>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
